e908868c431bf82562be9fd6b5682ed70b190ec3
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2025-06-03 20:12:55 +0600

    Update

---
+++ src/main/java/net/javaguides/springboot/helloworld.java
@@ -0,0 +1,11 @@
+package net.javaguides.springboot;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
